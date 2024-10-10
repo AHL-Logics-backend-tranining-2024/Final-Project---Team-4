d57f8f1d42076becab4ac27f3f02e8ec9d9aadf5
FatmaQunnies24 <fatima.n.qunnies@gmail.com>
2024-10-10 16:19:40 +0300

    product

--- app/api/main.py
+++ app/api/main.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
-from app.api.routes import user, login, status, order
+
+from app.api.routes import user, login, status, order,product_routes
+# from app.api.routes.product_routes import router as product_router
+
 
 api_router = APIRouter()
 api_router.include_router(login.router, prefix="/login", tags=["login"])
 api_router.include_router(user.router, prefix="/users", tags=["users"])
 api_router.include_router(order.router, prefix="/orders", tags=["orders"])
+api_router.include_router(product_routes.router, prefix="/products", tags=["products"])
